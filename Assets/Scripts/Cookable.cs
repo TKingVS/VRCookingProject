c51b151e23d46a1a45e9075aa46d162234917ae7
Thomas King <37191454+TKingVS@users.noreply.github.com>
2018-10-25 21:14:03 -0700

    Added food objects, fixed pan colliders, started working on cooking script.

---
+++ Assets/Scripts/Cookable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cookable : MonoBehaviour {
+
+	public Material mat;
+
+	// Use this for initialization
+	void Start () {
+		mat = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mat.color = new Color(0.848f, 0.472f, 0, 1);
+	}
+}
---
+++ Assets/Scripts/PanController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanController : MonoBehaviour {
+
+	public Vector3 com;
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.centerOfMass = com;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
