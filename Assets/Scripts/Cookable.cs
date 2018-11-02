fab332ac676de3bf3bb71d731bbf5c3fded9e879
Ellen Tseng <33066419+elltseng@users.noreply.github.com>
2018-11-02 12:51:30 -0700

    Worked on Oculus integration

--- Assets/Scripts/Cookable.cs
+++ Assets/Scripts/Cookable.cs
@@ -8,33 +8,35 @@ public class Cookable : MonoBehaviour {
 	public Collider col_top, col_bot;
 	public Color cooked_color = new Color(0.848f, 0.472f, 0, 1);
 
+	private Color start_color;
 	private float cook_timer_top, cook_timer_bot;
 	private float cook_time;
 
 	// Use this for initialization
 	void Start() {
+		start_color = mat_top.color;
 		cook_timer_top = cook_timer_bot = 0.0f;
 		cook_time = 10.0f;
 	}
-	
+
 	// Update is called once per frame
 	void Update() {
 
 	}
 
 	void OnDestroy() {
-		mat_top.color = Color.white;
-		mat_bot.color = Color.white;
+		mat_top.color = start_color;
+		mat_bot.color = start_color;
 	}
 
 	void Cook(Collider trigger) {
 		if (trigger == col_top && cook_timer_top <= cook_time) {
 			cook_timer_top += Time.fixedDeltaTime;
-			mat_top.color = Vector4.Lerp(Color.white, cooked_color, cook_timer_top / cook_time);
+			mat_top.color = Vector4.Lerp(start_color, cooked_color, cook_timer_top / cook_time);
 		}
 		if (trigger == col_bot && cook_timer_bot <= cook_time) {
 			cook_timer_bot += Time.fixedDeltaTime;
-			mat_bot.color = Vector4.Lerp(Color.white, cooked_color, cook_timer_bot / cook_time);
+			mat_bot.color = Vector4.Lerp(start_color, cooked_color, cook_timer_bot / cook_time);
 		}
 	}
 }
